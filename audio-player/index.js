809c18d5d34b43a3edbfbb26cea7c36135a24411
Darya Kolenchenko <daria.kolenchenko@gmail.com>
2024-09-11 15:14:32 +0200

    feat: modify styles

---
+++ audio-player/index.js
@@ -0,0 +1,22 @@
+const songs = [
+    {
+        name: 'Numb',
+        band: 'Linkin Park',
+        src: './assets/audio/Numb.mp3',
+        cover: './assets/img/numb.jpg',
+    },
+    {
+        name: 'Reality',
+        band: 'Lost Frequencies',
+        src: './assets/audio/Reality.mp3',
+        cover: './assets/img/reality.jpg',
+    },
+    {
+        name: 'Therapy',
+        band: 'All Time Low',
+        src: './assets/audio/Therapy.mp3',
+        cover: './assets/img/therapy.jpg',
+    }, 
+]
+
+const audio = document.querySelector("audio");
