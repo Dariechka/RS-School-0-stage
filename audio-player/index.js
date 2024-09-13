ef6fa92006134f9e11c8fcb885589397b6720bca
Darya Kolenchenko <daria.kolenchenko@gmail.com>
2024-09-13 13:02:52 +0200

    feat: finish task

--- audio-player/index.js
+++ audio-player/index.js
@@ -4,22 +4,22 @@ const songs = [
         band: 'Linkin Park',
         src: './assets/audio/Numb.mp3',
         cover: './assets/img/numb.jpg',
-        background: '../assets/img/numbBackground.jpg',
+        background: './assets/img/numbBackground.jpg',
     },
     {
         name: 'Reality',
         band: 'Lost Frequencies',
         src: './assets/audio/Reality.mp3',
         cover: './assets/img/reality.jpg',
-        background: '../assets/img/realityBackground.jpg',
+        background: './assets/img/realityBackground.jpg',
     },
     {
         name: 'Therapy',
         band: 'All Time Low',
         src: './assets/audio/Therapy.mp3',
         cover: './assets/img/therapy.jpg',
-        background: '../assets/img/therapyBackground.jpg',
-    }, 
+        background: './assets/img/therapyBackground.jpg',
+    },
 ]
 
 const audio = document.querySelector('audio');
@@ -33,82 +33,92 @@ const previousButton = document.querySelector('.player__buttons_right');
 const songName = document.querySelector('.player__song');
 const singerName = document.querySelector('.player__singer');
 const imgSong = document.querySelector('.imgSong');
-let isPlay = false;
+
 let number = 0;
 
 function playAudio() {
-    if (!isPlay) {
-        audio.play();
-        playButton.style.display = 'none';
-        pauseButton.style.display = 'flex';
-        isPlay = true;
-    } else {
-        audio.pause();
-        playButton.style.display = 'flex';
-        pauseButton.style.display = 'none';
-        isPlay = false;
-    }
+    audio.play();
+    playButton.style.display = 'none';
+    pauseButton.style.display = 'flex';
+}
+
+function pauseAudio() {
+    audio.pause();
+    playButton.style.display = 'flex';
+    pauseButton.style.display = 'none';
 }
 
 function formateTime(time) {
-    let sec = Math.floor(time % 60).toString().padStart(2, '0');
-    let min = Math.floor(time / 60).toString().padStart(2, '0');
-    return `${min}:${sec}`
+    const sec = Math.floor(time % 60).toString().padStart(2, '0');
+    const min = Math.floor(time / 60).toString().padStart(2, '0');
+    return `${min}:${sec}`;
+}
+
+function updateProgressBar() {
+    const currentTime = audio.currentTime || 0;
+    const duration = audio.duration || 0;
+
+    currentTimeValue.innerHTML = formateTime(currentTime);
+    durationTimeValue.innerHTML = formateTime(duration);
+
+    const progress = (currentTime / duration) || 0;
+    progressBar.value = progress * 100;
+    progressBar.style = 'background-image: -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(' + progress + ', #E9DCC9), color-stop(' + progress + ', #202639));';
+}
+
+function makePlayer(song) {
+    number = songs.indexOf(song);
+
+    audio.src = song.src;
+    imgSong.src = song.cover;
+    document.body.style.backgroundImage = `url(${song.background})`;
+    songName.innerHTML = song.name;
+    singerName.innerHTML = song.band;
+
+    updateProgressBar();
+}
+
+function playNext() {
+    const nextNumber = number + 1;
+    if (nextNumber === songs.length) {
+        makePlayer(songs[0]);
+    } else {
+        makePlayer(songs[nextNumber]);
+    }
+    playAudio();
+}
+
+function playPrevious() {
+    const previousNumber = number - 1;
+    if (previousNumber < 0) {
+        makePlayer(songs[songs.length - 1]);
+    } else {
+        makePlayer(songs[previousNumber]);
+    }
+    playAudio();
 }
 
 playButton.addEventListener('click', playAudio);
-pauseButton.addEventListener('click', playAudio);
+pauseButton.addEventListener('click', pauseAudio);
 
 progressBar.addEventListener('input', (event) => {
     const seekTime = (audio.duration / 100) * event.target.value;
     audio.currentTime = seekTime;
 });
-  
+
 audio.addEventListener('timeupdate', () => {
-    const progress = (audio.currentTime / audio.duration) * 100;
-    progressBar.value = progress;
+    updateProgressBar();
 
-
-    currentTimeValue.innerHTML = '';
-    durationTimeValue.innerHTML = '';
-   
-    currentTimeValue.innerHTML = formateTime(audio.currentTime);
-    durationTimeValue.innerHTML = formateTime(audio.duration);
-
-    let valPercent = progressBar.valueAsNumber / 100;
-    progressBar.style = 'background-image: -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop('+ valPercent+', #E9DCC9), color-stop('+ valPercent+', #202639));';
-
-    if (audio.currentTime == audio.duration){
-        playAudio();
+    if (audio.currentTime == audio.duration) {
+        playNext();
     }
 });
 
-nextButton.addEventListener('click', function(){
-    number += 1;
-    makePlayList(number);
-})
+nextButton.addEventListener('click', playNext);
+previousButton.addEventListener('click', playPrevious);
 
-previousButton.addEventListener('click', function(){
-    number -= 1;
-    makePlayList(number);
-})
+audio.addEventListener('loadedmetadata', () => {
+    updateProgressBar();
+});
 
-
-function makePlayList (number) {
-    if (number === songs.length) {
-        number = 0;
-    }
-
-    audio.src = songs[number].src;
-    imgSong.src = songs[number].cover;
-    document.body.style.backgroundImage = `url(${songs[number].background})`;
-    songName.innerHTML = '';
-    singerName.innerHTML = '';
-    songName.innerHTML = songs[number].name;
-    singerName.innerHTML = songs[number].band;
-
-    playAudio();
-    number += 1;
-};
-
-makePlayList(number);
+makePlayer(songs[number]);
